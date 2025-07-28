89f57445185e89921991e9b606951f34f85c26c5
Santanu Bhattacharyya <santanu@Santanus-MacBook-Pro.local>
2025-07-28 23:42:55 +0530

    First Spring AI application with Open AI

---
+++ spring-ai-1-chat-app/src/main/java/com/ai/chat/springai/SpringAi1ChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.ai.chat.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAi1ChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAi1ChatAppApplication.class, args);
+    }
+
+}
