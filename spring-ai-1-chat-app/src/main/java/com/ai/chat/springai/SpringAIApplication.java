79128a757054f8feb855dd4d84ff1cc22a0d79bd
Santanu Bhattacharyya <santanu@Santanus-MacBook-Pro.local>
2025-08-07 22:15:13 +0530

    Completed initial AI chat application

--- spring-ai-1-chat-app/src/main/java/com/ai/chat/springai/SpringAIApplication.java
+++ spring-ai-1-chat-app/src/main/java/com/ai/chat/springai/SpringAIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAi1ChatAppApplication {
+public class SpringAIApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringAi1ChatAppApplication.class, args);
+        SpringApplication.run(SpringAIApplication.class, args);
     }
 
 }
