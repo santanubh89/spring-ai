2576aafda990601654182ced62924777d5f06049
Santanu Bhattacharyya <santanu@Santanus-MacBook-Pro.local>
2025-08-09 14:05:59 +0530

    RAG architecture with PG Vector

---
+++ spring-ai-4-rag-pgvector/src/test/java/com/ai/springai/springai4ragpgvector/SpringAi4RagPgvectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.springai.springai4ragpgvector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAi4RagPgvectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
