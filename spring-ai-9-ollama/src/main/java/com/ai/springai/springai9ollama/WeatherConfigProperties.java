7155755836e82519e281be9831bfe56609dd3826
santanubh89 <santanu.bh6@gmail.com>
2025-08-10 16:30:56 +0530

    Delete spring-ai-9-ollama directory

--- spring-ai-9-ollama/src/main/java/com/ai/springai/springai9ollama/WeatherConfigProperties.java
+++
@@ -1,7 +0,0 @@
-package com.ai.springai.springai9ollama;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "weather")
-public record WeatherConfigProperties(String apiKey, String apiUrl) {
-}
